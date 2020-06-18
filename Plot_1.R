a5fd806ab0d34e558123842132a0aef6086e45a6
anfsierrari <anfsierraric@gmail.com>
2020-06-18 16:51:31 -0500

    First plot and it script

---
+++ Plot_1.R
@@ -0,0 +1,14 @@
+
+library(dplyr)
+#Read the data
+my_data <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
+my_data <- mutate(my_data, Date = as.Date(as.character(my_data$Date),
+                                          format="%d/%m/%Y"))
+#Subset only the dates 2007-02-01 and 2007-02-02
+my_data <- my_data[my_data$Date == "2007-02-01" | my_data$Date == "2007-02-02", ]
+#Making the histogram
+my_data <- my_data$Global_active_power <- as.numeric(asc.character(my_data$Global_active_power))
+hist(my_table$Global_active_power, col="red", 
+     xlab="Global Active Power(kilowatts)", main = "Global Active Power")
+dev.copy(png, "./plot_1.png")
+dev.off()
